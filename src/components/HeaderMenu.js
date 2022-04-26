9cbdfe85228e976b7d27265e95f19ffa9a74edd1
batsWTR <baptiste.wentzler@wanadoo.fr>
2022-04-26 16:02:23 +0200

    mise en forme

--- src/components/HeaderMenu.js
+++ src/components/HeaderMenu.js
@@ -6,7 +6,7 @@ function HeaderMenu(){
         <div className="logo">
             <i><svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false"  ><g ><path d="M21,6H3V5h18V6z M21,11H3v1h18V11z M21,17H3v1h18V17z" ></path></g></svg></i>
             <div>
-                <img src={logoSvg} alt="logo YoutBats" />
+                <img src={logoSvg} alt="logo YoutBats" title="Accueil YouBats" />
             </div>
             
         </div>
