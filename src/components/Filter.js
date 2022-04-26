60c102ce82100516b1c7f9dc467dcfac9bd5af2d
batsWTR <baptiste.wentzler@wanadoo.fr>
2022-04-26 16:44:11 +0200

    function filter

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -5,15 +5,31 @@ import ButtonFilter from './ButtonFilter';
 
 
 function Filter(){
-    const items = ['Tous', 'Expériences', 'Réalisations', 'Diplômes', 'Hobbies'];
+    const items = [
+        {name:'Tous', isActive: true}, 
+        {name:'Expériences', isActive: false}, 
+        {name:'Réalisations', isActive: false}, 
+        {name:'Diplômes', isActive: false}, 
+        {name:'Hobbies', isActive: false}
+    ];
     const itemList =[];
 
     items.map((el,id) =>{
-         itemList.push(<ButtonFilter name={ el } key={el+id} />);
+         itemList.push(<ButtonFilter name={ el.name } isActive={ el.isActive} key={el.name+id} />);
     });
     
     function handleClick(ev){
         console.log(ev.target.title);
+        console.log(items);
+        items.map((el,id,ar)=>{
+            if(el.name === ev.target.title){
+                ar[id].isActive = true;
+            }
+        });
+
+        items.map((el,id) =>{
+            itemList.push(<ButtonFilter name={ el.name } isActive={ el.isActive} key={el.name+id} />);
+       });
     };
 
     return(
