273a7e486e5e7bee565554cd415ccec0bf33ef17
batsWTR <baptiste.wentzler@wanadoo.fr>
2022-04-26 12:05:13 +0200

    ajout logo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import Filter from './components/Filter';
+import MenuLarge from './components/MenuLarge'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <div className='content'>
-        <div className='content-icone'>Icones</div>
+        <MenuLarge />
         <div className='content-main'>
           <Filter />
           <div>Contenu cartes</div>
