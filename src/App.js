3ba68d5716c3a49188ab9d6500497ba860ff9478
batsWTR <baptiste.wentzler@wanadoo.fr>
2022-04-27 10:11:16 +0200

    ajout descriptifs cartes

--- src/App.js
+++ src/App.js
@@ -14,8 +14,15 @@ const items = [
 ];
 
 const cards = [
-  {title:"Belle o natur'elle", view:"10K"},
-  {title:"O'cado", view:"30K"}
+  {title:"Belle o natur'elle",image:"#", view:"10K"},
+  {title:"O'cado", image:"#", view:"30K"},
+  {title:"Armée de terre", image:"#", category:"Expériences", view:"1998-2003"},
+  {title:"Technicien télécom Gobé", image:"#", category:"Expériences", view:"2003-2008"},
+  {title:"Technicien télécom SDIS", image:"#", category:"Expériences", view:"2009"},
+  {title:"Technicien télécom SPIE", image:"#", category:"Expériences", view:"2010-2012"},
+  {title:"Technicien radio CERN", image:"#", category:"Expériences", view:"2012-2014"},
+  {title:"Technicien boucle locale Eiffage", image:"#", category:"Expériences", view:"2016"},
+  {title:"Technicien SAV télécom Alp'com", image:"#", category:"Expériences", view:"2016-2020"}
 ]
 
 
